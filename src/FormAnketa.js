0d73c2a1ffa8042bc83be80762d47fc76275e889
yfarhod <yfarhod75@gmail.com>
2022-03-06 19:07:32 +0100

    upd

---
+++ src/FormAnketa.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { View, TextInput, Button, StyleSheet } from "react-native";
+import { Formik } from "formik";
+
+export const FormAnketa = () => {
+  return <View></View>;
+};
