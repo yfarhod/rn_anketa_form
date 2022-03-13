047844682a0b917d88f68cbe8214a3ac8c16e04c
yfarhod <yfarhod75@gmail.com>
2022-03-13 22:05:25 +0100

    upd

--- src/FormAnketa.js
+++ src/FormAnketa.js
@@ -3,5 +3,14 @@ import { View, TextInput, Button, StyleSheet } from "react-native";
 import { Formik } from "formik";
 
 export const FormAnketa = () => {
-  return <View></View>;
+  return (
+    <View>
+      <Formik
+        initialValues={{ anketa: "", vopros1: "", vopros2: "", vopros3: "" }}
+        onSubmit={(values) => {}}
+      >
+        {(props) => <View></View>}
+      </Formik>
+    </View>
+  );
 };
