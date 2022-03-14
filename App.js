003d933c0c65e0ada9b86f97ea7c055b6f0d2a96
yfarhod <yfarhod75@gmail.com>
2022-03-14 18:44:58 +0100

    upd

--- App.js
+++ App.js
@@ -1,6 +1,5 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
-import "bootstrap/dist/css/bootstrap.css";
 
 import { FormAnketa } from "./src/formAnketa";
 
