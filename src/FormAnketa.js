edd3c6f168659abaf9ceb20a706822e7e27fbc25
yfarhod <yfarhod75@gmail.com>
2022-03-14 11:42:38 +0100

    formik fields added

--- src/FormAnketa.js
+++ src/FormAnketa.js
@@ -9,7 +9,30 @@ export const FormAnketa = () => {
         initialValues={{ anketa: "", vopros1: "", vopros2: "", vopros3: "" }}
         onSubmit={(values) => {}}
       >
-        {(props) => <View></View>}
+        {(props) => (
+          <View>
+            <TextInput
+              placeholder="номер анкеты"
+              onChangeText={props.handleChange("anketa")}
+              value={props.values.anketa}
+            />
+            <TextInput
+              placeholder="ответ 1"
+              onChangeText={props.handleChange("vopros1")}
+              value={props.values.vopros1}
+            />
+            <TextInput
+              placeholder="ответ 2"
+              onChangeText={props.handleChange("vopros2")}
+              value={props.values.vopros2}
+            />
+            <TextInput
+              placeholder="ответ 3"
+              onChangeText={props.handleChange("vopros3")}
+              value={props.values.vopros3}
+            />
+          </View>
+        )}
       </Formik>
     </View>
   );
