53356454fe213cfd3e3c5f473611c3b75bc2725d
yfarhod <yfarhod75@gmail.com>
2022-03-22 00:10:27 +0100

    upd

--- src/FormAnketa.js
+++ src/FormAnketa.js
@@ -1,59 +1,67 @@
 import React from "react";
-import { View, StyleSheet } from "react-native";
-import { TextInput, Button } from "react-native-paper";
+import {
+  View,
+  StyleSheet,
+  TouchableWithoutFeedback,
+  Keyboard,
+} from "react-native";
+import { TextInput, Button, Text } from "react-native-paper";
 import { Formik } from "formik";
 
 export const FormAnketa = () => {
   return (
-    <View style={styles.container}>
-      <Formik
-        initialValues={{ anketa: "", vopros1: "", vopros2: "", vopros3: "" }}
-        onSubmit={(values) => {
-          console.log(values);
-        }}
-      >
-        {(props) => (
-          <View>
-            <Text>Номер анкеты</Text>
-            <TextInput
-              style={styles.inputField}
-              placeholder="номер анкеты"
-              onChangeText={props.handleChange("anketa")}
-              value={props.values.anketa}
-            />
-            <Text>Вопрос 1</Text>
-            <TextInput
-              style={styles.inputField}
-              placeholder="ответ 1"
-              onChangeText={props.handleChange("vopros1")}
-              value={props.values.vopros1}
-            />
-            <Text>Вопрос 2</Text>
-            <TextInput
-              style={styles.inputField}
-              placeholder="ответ 2"
-              onChangeText={props.handleChange("vopros2")}
-              value={props.values.vopros2}
-            />
-            <Text>Вопрос 3</Text>
-            <TextInput
-              style={styles.inputField}
-              placeholder="ответ 3"
-              onChangeText={props.handleChange("vopros3")}
-              value={props.values.vopros3}
-            />
-            <Button
-              style={styles.inputField}
-              mode="contained"
-              color="green"
-              onPress={props.handleSubmit}
-            >
-              Добавить
-            </Button>
-          </View>
-        )}
-      </Formik>
-    </View>
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <View style={styles.container}>
+        <Formik
+          initialValues={{ anketa: "", vopros1: "", vopros2: "", vopros3: "" }}
+          onSubmit={(values, actions) => {
+            console.log(values);
+            actions.resetForm();
+          }}
+        >
+          {(props) => (
+            <View>
+              <Text style={styles.inputField}>Номер анкеты</Text>
+              <TextInput
+                style={styles.inputField}
+                placeholder="номер анкеты"
+                onChangeText={props.handleChange("anketa")}
+                value={props.values.anketa}
+              />
+              <Text style={styles.inputField}>Вопрос 1</Text>
+              <TextInput
+                style={styles.inputField}
+                placeholder="ответ 1"
+                onChangeText={props.handleChange("vopros1")}
+                value={props.values.vopros1}
+              />
+              <Text style={styles.inputField}>Вопрос 2</Text>
+              <TextInput
+                style={styles.inputField}
+                placeholder="ответ 2"
+                onChangeText={props.handleChange("vopros2")}
+                value={props.values.vopros2}
+              />
+              <Text style={styles.inputField}>Вопрос 3</Text>
+              <TextInput
+                style={styles.inputField}
+                placeholder="ответ 3"
+                onChangeText={props.handleChange("vopros3")}
+                value={props.values.vopros3}
+              />
+              <Button
+                style={styles.inputField}
+                mode="contained"
+                color="green"
+                onPress={props.handleSubmit}
+              >
+                Добавить
+              </Button>
+            </View>
+          )}
+        </Formik>
+      </View>
+    </TouchableWithoutFeedback>
   );
 };
 
@@ -63,6 +71,7 @@ const styles = StyleSheet.create({
     backgroundColor: "white",
   },
   inputField: {
-    margin: 10,
+    marginHorizontal: 20,
+    marginVertical: 6,
   },
 });
