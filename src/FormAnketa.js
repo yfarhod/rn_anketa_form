9de63e4ade0a6e4803e0e33e757c49f00a74762e
yfarhod <yfarhod75@gmail.com>
2022-03-21 21:58:38 +0100

    upd

--- src/FormAnketa.js
+++ src/FormAnketa.js
@@ -14,24 +14,28 @@ export const FormAnketa = () => {
       >
         {(props) => (
           <View>
+            <Text>Номер анкеты</Text>
             <TextInput
               style={styles.inputField}
               placeholder="номер анкеты"
               onChangeText={props.handleChange("anketa")}
               value={props.values.anketa}
             />
+            <Text>Вопрос 1</Text>
             <TextInput
               style={styles.inputField}
               placeholder="ответ 1"
               onChangeText={props.handleChange("vopros1")}
               value={props.values.vopros1}
             />
+            <Text>Вопрос 2</Text>
             <TextInput
               style={styles.inputField}
               placeholder="ответ 2"
               onChangeText={props.handleChange("vopros2")}
               value={props.values.vopros2}
             />
+            <Text>Вопрос 3</Text>
             <TextInput
               style={styles.inputField}
               placeholder="ответ 3"
